e547d557a9b42827866781469398d4231722a8b9
Kauan Eduardo <kauan.eduardosilveira3@gmail.com>
2026-04-10 11:44:15 -0300

    12 - finalizando a IoC e publicando a aplicação

--- src/main/java/br/ifmg/produto1_2026/config/ProdutosConfig.java
+++ src/main/java/br/ifmg/produto1_2026/config/ProdutosConfig.java
@@ -1,22 +1,32 @@
 package br.ifmg.produto1_2026.config;
 
+import br.ifmg.produto1_2026.anotacoes.TipoDoNotificador;
+import br.ifmg.produto1_2026.constants.TipoDeNotificacao;
 import br.ifmg.produto1_2026.service.AtivacaoClienteService;
 import br.ifmg.produto1_2026.util.NotificacaoEmail;
 import br.ifmg.produto1_2026.util.NotificacaoSMS;
 import br.ifmg.produto1_2026.util.Notificador;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class ProdutosConfig {
 
+    @Value("${notificador.email.host}")
+    private String servidorSMTP;
+
     //Nesse caso a criação do bean é necessario, pois o Spring Boot não saberia criar esse objeto.
     //Qual seria o servidor smtp??
+    @Profile("prod")
+    @TipoDoNotificador(value = TipoDeNotificacao.EMAIL)
     @Bean
     public Notificador notificacaoEmail(){
 
-        NotificacaoEmail notificacaoEmail = new NotificacaoEmail("smtp.google.com");
+        NotificacaoEmail notificacaoEmail = new NotificacaoEmail(servidorSMTP);
         notificacaoEmail.setCaixaAlta(true);
         return notificacaoEmail;
 
@@ -33,6 +43,8 @@ public class ProdutosConfig {
 
 
     //@Primary -- desambigua beans, indicando qual objeto o spring boot deve usar.
+    @Profile("dev")
+    @TipoDoNotificador(value = TipoDeNotificacao.SMS)
     @Bean
     public Notificador notificacaoSMS(){
 
